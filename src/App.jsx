76a6770bc0fe0a27574977b34e042e8bf97b0243
soyboy <sbvegan@protonmail.com>
2022-11-06 13:38:21 -0800

    adding connect functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import {
   configureChains, 
   chain 
 } from "wagmi"
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { publicProvider } from "wagmi/providers/public";
 
 import NavBar from "./components/NavBar"
@@ -27,6 +31,28 @@ const { chains, provider, webSocketProvider } = configureChains(
 
 const client = createClient({
   autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
   provider,
   webSocketProvider
 })
--- src/components/NavBar/ConnectWalletButton.jsx
+++ src/components/NavBar/ConnectWalletButton.jsx
@@ -1,3 +1,10 @@
+import {
+    useAccount,
+    useConnect,
+    useDisconnect,
+    useEnsAvatar,
+    useEnsName,
+} from 'wagmi'
 import styled from "styled-components";
 
 const Button = styled.button`
@@ -6,7 +13,6 @@ const Button = styled.button`
   border-radius: 100px;
   box-sizing: border-box;
   color: rgb(255, 255, 255);
-  
   font-size: 14px;
   font-weight: 700;
   height: 40px;
@@ -25,15 +31,49 @@ const Button = styled.button`
 
 
 const ConnectWalletButton = () => {
+    const { address, connector, isConnected } = useAccount()
+    const { data: ensAvatar } = useEnsAvatar({ addressOrName: address })
+    const { data: ensName } = useEnsName({ address })
+    const { connect, connectors, error, isLoading, pendingConnector } = useConnect()
+    const { disconnect } = useDisconnect()
 
     const handleConnect = () => {
         console.log("Click")
     }
 
+    if (isConnected) {
+        return (
+            <div>
+            <img src={ensAvatar} alt="ENS Avatar" />
+            <div>{ensName ? `${ensName} (${address})` : address}</div>
+            <div>Connected to {connector.name}</div>
+            <button onClick={disconnect}>Disconnect</button>
+            </div>
+        )
+    }
+
+    // todo: refactor into a modal and integrate the connect wallet button
+
     return(
-        <Button onClick={handleConnect}>
-            Connect Wallet
-        </Button>
+        <div>
+            {connectors.map((connector) => (
+                <button
+                    disabled={!connector.ready}
+                    key={connector.id}
+                    onClick={() => connect({ connector })}
+                >
+                    {connector.name}
+                    {!connector.ready && ' (unsupported)'}
+                    {isLoading &&
+                    connector.id === pendingConnector?.id &&
+                    ' (connecting)'}
+                </button>
+            ))}
+            {error && <div>{error.message}</div>}
+            <Button onClick={handleConnect}>
+                Connect Wallet
+            </Button>
+        </div>
     )
 }
 
