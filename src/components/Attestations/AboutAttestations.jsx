3c3808c93d22e10dff28d7ac06c8d47c07ac0e68
soyboy <sbvegan@protonmail.com>
2022-11-28 22:28:05 -0800

    updating about section

--- src/components/Attestations/AboutAttestations.jsx
+++ src/components/Attestations/AboutAttestations.jsx
@@ -24,20 +24,54 @@ const Text = styled.p`
 
 `
 
+const Link = styled.a`
+  color: #f01a37;
+`
+
 // TODO: provide more helpful information
 
 const AboutAttestations = () => {
   return (
-        <AboutContainer>
-            <SubSection>What&apos;s this?</SubSection>
-            <Text>An open-source interface for the Attestation Station.</Text>
-            <SubSection>What&apos;s the Attestation Station?</SubSection>
-            <Text>The Attestation Station is a smart contract deployed on Optimism. It allows anyone to make arbitrary attestations about</Text>
-            <SubSection>Why?</SubSection>
-            <Text>To build out a reputation and identity layer on-chain.</Text>
-            <SubSection>Learn more</SubSection>
-            <Text>TODO: add links</Text>
-        </AboutContainer>
+    <AboutContainer>
+        <SubSection>What&apos;s this?</SubSection>
+        <Text>
+          An open-source interface for the Attestation Station. If you&apos;d like to make a contribution or report an issue; please head over to the&nbsp;
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/sbvegan/attestation-station-interface"
+          >
+            Github repository
+          </Link>
+        </Text>
+
+        <SubSection>What&apos;s the Attestation Station?</SubSection>
+        <Text>
+          The Attestation Station is a smart contract deployed on Optimism. It allows anyone to make arbitrary attestations about Ethereum addresses.
+        </Text>
+
+        <SubSection>Why?</SubSection>
+        <Text>
+          Attestations can be used for a wide variety of applications including voting, reputations scores, reviews, and more. As an example, attestations written to the AttestationStation form the base layer of reputation for the&nbsp;
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://community.optimism.io/docs/governance/citizens-house/"
+          >
+            Optimism Citizen House
+          </Link>
+        </Text>
+        <SubSection>Learn more</SubSection>
+        <Text>
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://community.optimism.io/docs/governance/attestation-station/#"
+          >
+              Official Optimism Documentation
+          </Link>
+        </Text>
+    </AboutContainer>
   )
 }
 
